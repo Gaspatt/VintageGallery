be10f4e529c9a862b0a1fbd0e3b6c9af8b309af5
gaspatt <matheusgaspar29.if@gmail.com>
2024-11-01 11:53:08 -0300

    feat:Alteração no home #2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeaderHome from './components/HeaderHome.vue';
+import HomePage from './components/HomePage.vue';
 
 </script>
 
 <template>
 
   <HeaderHome></HeaderHome>
+  <HomePage></HomePage>
 
 </template>
 
---
+++ src/components/HomePage.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+
+<template>
+ <section id="secback">
+        <div class="homeback">
+
+  </div>
+    </section>
+  <main>
+   
+    <div>fkasjdk fkasjdfk kfjaskd</div>
+  </main>
+</template>
+
+<style scoped>
+.homeback {
+  position: sticky;
+  top: 0;
+  z-index: -10;
+  width: 100%;
+  height: 60%;
+}
+
+#secback {
+    margin: 0;
+    border-radius: 0;
+    background: url(/src/assets/imagens/homeback.png);
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+    height: 100vh;
+}
+
+.homeback img {
+  width: 100%;
+  height: 60%;
+}
+
+main {
+  /* position: sticky; */
+  top: 0;
+}
+</style>
