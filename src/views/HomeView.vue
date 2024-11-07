d1b3159cdd5d3294f1a97fa34326c094ce8d789d
gaspatt <matheusgaspar29.if@gmail.com>
2024-11-07 11:46:17 -0300

    feat:Criação do FooterPage #1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeaderHome from '@/components/HeaderHome.vue';
 import HomePage from '@/components/HomePage.vue';
+import FooterHome from '@/components/FooterHome.vue';
 
 </script>
 
 <template>
 <HeaderHome></HeaderHome>
 <HomePage></HomePage>
+<FooterHome></FooterHome>
 
 </template>
