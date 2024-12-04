bd512bc888dac92cca2855af498d9be57697e5d8
ManoelFranciscoDias <manoel2007dias@gmail.com>
2024-12-04 19:30:41 -0300

    feat: criando pagina de usuario #7

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -18,13 +18,20 @@ async function logout() {
 </script>
 
 <template>
-  <div>
-    <h1>Perfil do Usuário</h1>
+  <div class="container">
+    <h1>PERFIL</h1>
+    <P>Suas informações de usuarios abaixo:</P>
+
+
+    <i class="fa-solid fa-user"></i>
+
+
+
     <div v-if="user">
-      <p><strong>Nome:</strong> {{ user.name }}</p>
-      <p><strong>Email:</strong> {{ user.email }}</p>
-      <p><strong>Data de Nascimento:</strong> {{ user.birthdate }}</p>
-      <button @click="logout">Sair</button>
+      <p class="input1"><strong>Nome:</strong> {{ user.name }}</p>
+      <p class="input"><strong>Email:</strong> {{ user.email }}</p>
+      <p class="input"><strong>Data de Nascimento:</strong> {{ user.birthdate }}</p>
+      <button @click="logout">DESCONECTAR</button>
     </div>
     <div v-else>
       <p>blablablabla...</p>
@@ -33,5 +40,55 @@ async function logout() {
 </template>
 
 <style scoped>
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+}
+
+i {
+  font-size: 100px;
+  margin-top: 30px;
+}
+
+.input1 {
+  margin-top: 30px;
+  background-color: rgb(214, 214, 214);
+  font-size: 15pt;
+  padding: 5px;
+  width: 500px;
+  border-radius: 5px;
+}
+
+.input {
+
+  background-color: rgb(214, 214, 214);
+  font-size: 15pt;
+  padding: 5px;
+  width: 500px;
+  border-radius: 5px;
+}
+
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: black;
+  color: white;
+  padding: 5px;
+  width: 500px;
+  border-radius: 5px;
+  border: none;
+  transition: 0.3s;
+  margin-top: 30px;
+}
+
+button:hover {
+  background-color: rgb(65, 64, 64);
+}
 
 </style>
